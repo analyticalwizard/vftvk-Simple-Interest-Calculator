fe08013e9d351a4033941d562dfbc8e39d1eed15
analyticalwizard <z5231519@ad.unsw.edu.au>
2021-04-07 20:42:56 +0000

    ok

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function compute()
 {
     var principal=document.getElementById("principal").value;
-    var rate=document.getElementById("rate").value;
+    var rate=document.getElementById("interest_rate").value;
     var years =document.getElementById("years").value;
     var interest = principal*years*rate/100;
 
